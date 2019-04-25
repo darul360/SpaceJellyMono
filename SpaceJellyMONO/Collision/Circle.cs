395224f40e6d96cbfc782316ed582982209f92a8
Jedrzej <jejakx@gmail.com>
2019-04-26 00:17:21 +0200

    update

--- SpaceJellyMONO/Collision/Circle.cs
+++ SpaceJellyMONO/Collision/Circle.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 
 namespace SpaceJellyMONO.GameObjectComponents
 {
     class Circle : Collider
     {
-        //public CylinderPrimitive cylinder = new CylinderPrimitive()
+        private GraphicsDevice deviceManager;
+        public CylinderPrimitive cylinder;
         public float radius;
+        public Camera cam;
         public Circle(GameObject parent, float radius) : base(parent)
         {
             this.radius = radius;
             colliderTYPE = Type.circle;
+            deviceManager = parent.mainClass.GraphicsDevice;
+            cylinder = new CylinderPrimitive(deviceManager, 5, 2, 20);
+            cam = parent.camera;
         }
         public override bool Intersect(Collider other)
         {
@@ -27,7 +34,7 @@ namespace SpaceJellyMONO.GameObjectComponents
 
         public override void DrawCollider()
         {
-          //  cylinder.Draw()
+           // cylinder.Draw(parent.transform.World(), cam.View, cam.Projection, new Color(0, 0, 225));
         }
     }
 }
